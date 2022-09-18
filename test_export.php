abfb4729150811902928f66e2f7056487bd25eae
Jesse de Zwart <jessedezwart@live.nl>
2022-09-18 19:13:56 +0200

    Move path to constant

--- test_export.php
+++ test_export.php
@@ -2,6 +2,7 @@
 
 const TOTAL_RUNS = 6;
 const RESULTS_FILE_NAME = __DIR__ . '/results_export.txt';
+const MYSQL_PATH = "C:\/ProgramData/MySQL/MySQL Server 8.0/Uploads/";
 
 // connect to databases
 $connMariadb = mysqli_connect("localhost", "root", "toor", "test_data");
@@ -21,7 +22,7 @@ for ($i=0; $i < TOTAL_RUNS; $i++) {
 
     // test mysql time to export 500000 rows
     $start = microtime(true);
-    $result = mysqli_query($connMysql, "SELECT * FROM answers INTO OUTFILE 'C:\/ProgramData/MySQL/MySQL Server 8.0/Uploads/" .time() . ".csv' FIELDS ENCLOSED BY '\"' TERMINATED BY ',' ESCAPED BY '\"' LINES TERMINATED BY '\n';");
+    $result = mysqli_query($connMysql, "SELECT * FROM answers INTO OUTFILE '" . MYSQL_PATH . time() . ".csv' FIELDS ENCLOSED BY '\"' TERMINATED BY ',' ESCAPED BY '\"' LINES TERMINATED BY '\n';");
     $end = microtime(true);
     echo "Mysql: " . ($end - $start) . " seconds" . PHP_EOL;
     file_put_contents(RESULTS_FILE_NAME, "Mysql: " . ($end - $start) . PHP_EOL, FILE_APPEND);
